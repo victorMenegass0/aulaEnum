3cf267aef6b915e3063b863d7213e12c219e4ff8
victorMenegass0 <53906166+victorMenegass0@users.noreply.github.com>
2020-10-26 18:47:30 -0300

    first commit

---
+++ src/enumaula2/EnumAula2.java
@@ -0,0 +1,12 @@
+
+package enumaula2;
+
+public class EnumAula2 {
+
+    public static void main(String[] args) {
+        DiaSemana dia = DiaSemana.DOMINGO;
+        
+        System.out.println(dia.toString()+'-'+dia.getValor());
+    }
+    
+}
